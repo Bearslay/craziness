aeafd223060c07b2589df1cc1033f4aa129691cf
Bearslay <barfunkel@protonmail.com>
2024-04-11 21:53:30 -0400

    converted many functions to template functions

--- physics.cpp
+++ physics.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int main() {
+    bool useRadians = false;
     double y0 = 10;
     double yf = 20;
     double x0 = 0;
@@ -11,57 +12,34 @@ int main() {
     double dy = yf - y0;
     double mag = 34;
 
-    double a = kmtcs::launchAngle_Deg(dx, dy, mag);
+    double a = kmtcs::launchAngle<double>(dx, dy, mag, true, useRadians);
+    std::pair<double, double> landing = kmtcs::landingVector<double>(mag, a, dy, useRadians);
     if (isnanf(a)) {
         std::cout << "Can't reach target with given launch velocity\n";
         return -1;
     }
 
-    std::cout << "Change in Height (dy):    " << dy << " m\n";
-    std::cout << "Initial Height (y0):      " << y0 << " m\n";
-    std::cout << "Initial X-Position (x0):  " << x0 << " m\n\n";
+    std::cout << " Change in Position (dx): " << dx << " m\n";
+    std::cout << "       Final Height (yf): " << yf << " m\n";
+    std::cout << "   Final X-Position (xf): " << xf << " m\n\n";
 
-    std::cout << "Launch Velocity (V0):     " << mag << " m/s\n";
-    std::cout << "Launch Velocity X (V0x):  " << breakVector_Deg(mag, a).first << " m/s\n";
-    std::cout << "Launch Velocity Y (V0y):  " << breakVector_Deg(mag, a).second << " m/s\n";
-    std::cout << "Launch Angle:             " << a << " deg\n\n";
+    std::cout << "   Change in Height (dy): " << dy << " m\n";
+    std::cout << "     Initial Height (y0): " << y0 << " m\n";
+    std::cout << " Initial X-Position (x0): " << x0 << " m\n\n";
 
-    std::cout << "Change in Position (dx):  " << dx << " m\n";
-    std::cout << "Final Height (yf):        " << yf << " m\n";
-    std::cout << "Final X-Position (xf):    " << xf << " m\n\n";
+    std::cout << "    Launch Velocity (V0): " << mag << " m/s\n";
+    std::cout << " Launch Velocity X (V0x): " << breakVector<double>(mag, a, useRadians).first << " m/s\n";
+    std::cout << " Launch Velocity Y (V0y): " << breakVector<double>(mag, a, useRadians).second << " m/s\n";
+    std::cout << "            Launch Angle: " << a << " deg\n\n";
 
-    std::cout << "Landing Velocity (Vf):    " << kmtcs::landingVelocity_Deg(mag, a, dy) << " m/s\n";
-    std::cout << "Landing Velocity X (Vfx): " << breakVector_Deg(kmtcs::landingVelocity_Deg(mag, a, dy), kmtcs::landingAngle_Deg(mag, a, dy)).first << " m/s\n";
-    std::cout << "Landing Velocity Y (Vfy): " << breakVector_Deg(kmtcs::landingVelocity_Deg(mag, a, dy), kmtcs::landingAngle_Deg(mag, a, dy)).second << " m/s\n";
-    std::cout << "Landing Angle:            " << kmtcs::landingAngle_Deg(mag, a, dy) << " deg\n\n";
+    std::cout << "   Landing Velocity (Vf): " << landing.first << " m/s\n";
+    std::cout << "Landing Velocity X (Vfx): " << breakVector<double>(landing.first, landing.second, useRadians).first << " m/s\n";
+    std::cout << "Landing Velocity Y (Vfy): " << breakVector<double>(landing.first, landing.second, useRadians).second << " m/s\n";
+    std::cout << "           Landing Angle: " << landing.second << " deg\n\n";
 
-    std::cout << "Maximum Height:           " << kmtcs::maxHeight_Deg(mag, a, y0) << " m\n";
-    std::cout << "Time to Reach Max Height: " << kmtcs::peakTime_Deg(mag, a) << " sec\n";
-    std::cout << "Total Airtime:            " << kmtcs::airTime_Deg(mag, a, dy) << " sec\n\n";
-
-    Vector_3D<double> vr = Vector_3D<double>(5, 4, 3);
-    std::cout << vr.rToString_Length(5) << "\n";
-    std::cout << vr.sToString_Length(5) << "\n";
-    Vector_3D<double> vs = Vector_3D<double>(10, 30, 45, true);
-    std::cout << vs.sToString_Length(5) << "\n";
-    std::cout << vs.rToString_Length(5) << "\n\n";
-
-    Coord_3D<int> c1 = Coord_3D<int>(4, 7, 2);
-    std::cout << c1.toString_Places(2, 2) << "\n\n";
-
-    double num = 2124.24532;
-    double num1 = -0.0425334;
-    long double avagadro = 602000000000000000000000.0;
-    int num2 = 12;
-    std::cout << astr::toString_Places(num, 4, 4) << "\n";
-    std::cout << astr::toString_Places(astr::round(num, 3), 4, 4) << "\n\n";
-    std::cout << astr::toString(avagadro) << "\n";
-    std::cout << astr::toString_Sci(avagadro) << "\n";
-    std::cout << astr::toString(num1) << "\n";
-    std::cout << astr::toString_Sci(num1) << "\n";
-    std::cout << astr::toString_Sci(num1, 2, 2, false) << "\n";
-    std::cout << astr::toString(num2) << "\n";
-    std::cout << astr::toString_Sci(num2) << "\n";
+    std::cout << "          Maximum Height: " << kmtcs::maxHeight<double>(mag, a, y0, useRadians) << " m\n";
+    std::cout << "Time to Reach Max Height: " << kmtcs:: peakTime<double>(mag, a,     useRadians) << " sec\n";
+    std::cout << "           Total Airtime: " << kmtcs::  airTime<double>(mag, a, dy, useRadians) << " sec\n\n";
 
     return 0;
 }
--- physics.hpp
+++ physics.hpp
@@ -1,94 +1,58 @@
 #ifndef PHYSICS
 #define PHYSICS
 
-#include "astr.hpp"
-
-#include <string>
 #include <utility>
 #include <cmath>
-#include <vector>
 
-/**
- * Collection of constants that can be used in various equations (and changed too in some cases)
- */
 struct {
-    /**
-     * Acceleration due to gravity
-     */
     double g = 9.80665;
 } Constants;
 
-template <typename ArithType> std::pair<ArithType, double> makeVector(ArithType x, ArithType y, bool useRadians = true) {
+template <typename ArithType> std::pair<ArithType, double> makeVector(const ArithType &x, const ArithType &y, const bool &useRadians = true) {
     static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-    return {std::sqrt(x * x + y * y), std::atan(y / x) * (useRadians ? 1 : 180 / M_PI)};
+    return {std::is_integral<ArithType>::value ? std::round(std::sqrt(x * x + y * y)) : std::sqrt(x * x + y * y), std::atan2(y, x) * (useRadians ? 1 : 180 / M_PI)};
 }
-template <typename ArithType> std::pair<ArithType, ArithType> breakVector(ArithType mag, double dir, bool useRadians = true) {
+template <typename ArithType> std::pair<ArithType, ArithType> breakVector(const ArithType &mag, const double &angle, const bool &useRadians = true) {
     static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-    return {mag * std::cos(dir), mag * std::sin(dir) * (useRadians ? 1 : 180 / M_PI)};
+    if (std::is_integral<ArithType>::value) {return {std::round(mag * std::cos(angle * (useRadians ? 1 : 180 / M_PI))), std::round(mag * std::sin(angle * (useRadians ? 1 : 180 / M_PI)))};}
+    return {mag * std::cos(angle * (useRadians ? 1 : M_PI / 180)), mag * std::sin(angle * (useRadians ? 1 : M_PI / 180))};
 }
 
 namespace kmtcs {
-    double launchAngle_Rad(double dx, double dy, double mag, bool useMin = true) {
-        double angle1 = atan((pow(mag, 2) + sqrt(pow(mag, 4) - Constants.g * (Constants.g * pow(dx, 2) + 2 * dy * pow(mag, 2)))) / (Constants.g * dx));
-        double angle2 = atan((pow(mag, 2) - sqrt(pow(mag, 4) - Constants.g * (Constants.g * pow(dx, 2) + 2 * dy * pow(mag, 2)))) / (Constants.g * dx));
-        return useMin ? fmin(angle1, angle2) : fmax(angle1, angle2);
-    }
-    double launchAngle_Deg(double dx, double dy, double mag, bool useMin = true) {
-        return launchAngle_Rad(dx, dy, mag, useMin) * 180 / M_PI;
-    }
-
-    template <typename ArithType> ArithType launchAngle(ArithType dx, ArithType dy, ArithType mag, bool useMinAngle = true, bool useRadians = true) {
-        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-        const double root = std::sqrt(mag * mag * mag * mag - Constants.g * (Constants.g * dx * dx + 2 * dy * mag * mag));
-        const double a1 = std::atan((mag * mag + root) / (Constants.g * dx));
-        const double a2 = std::atan((mag * mag - root) / (Constants.g * dx));
-        const double output = (useMinAngle ? std::fmin(a1, a2) : std::fmax(a1, a2)) * (useRadians ? 1 : 180 / M_PI);
-        return std::is_integral<ArithType>::value ? std::round(output) : output;
-    }
-    
-    double airTime_Deg(double mag, double angle, double dy) {
-        double vy = mag * sin(angle * M_PI / 180);
-        double time1 = (-vy + sqrt(pow(vy, 2) - 2 * Constants.g * dy)) / -Constants.g;
-        double time2 = (-vy - sqrt(pow(vy, 2) - 2 * Constants.g * dy)) / -Constants.g;
-        return fmax(time1, time2);
-    }
-
-    template <typename ArithType> ArithType airTime(ArithType mag, double angle, ArithType dy, bool useRadians = true) {
+    template <typename ArithType> ArithType airTime(const ArithType &mag, const double &angle, const ArithType &dy, const bool &useRadians = true) {
         static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
         const double vy = mag * std::sin(angle * (useRadians ? 1 : M_PI / 180));
         const double root = std::sqrt(vy * vy - 2 * Constants.g * dy);
-        const double t1 = (-vy + root) / -Constants.g;
-        const double t2 = (-vy - root) / -Constants.g;
-        return std::is_integral<ArithType>::value ? std::round(std::fmax(t1, t2)) : std::fmax(t1, t2);
+        return std::is_integral<ArithType>::value ? std::round(std::fmax((-vy + root) / -Constants.g, (-vy - root) / -Constants.g)) : std::fmax((-vy + root) / -Constants.g, (-vy - root) / -Constants.g);
     }
 
-    double peakTime_Deg(double mag, double angle) {
-        return -(mag * sin(angle * M_PI / 180)) / -Constants.g;
-    }
-
-    template <typename ArithType> ArithType peakTime(ArithType mag, double angle, bool useRadians = true) {
-
-    }
-
-    double maxHeight_Deg(double mag, double angle, double y0) {
-        double peakTime = peakTime_Deg(mag, angle);
-        return y0 + (mag * sin(angle * M_PI / 180)) * peakTime - 0.5 * Constants.g * pow(peakTime, 2);
-    }
-
-    double landingAngle_Deg(double mag, double angle, double dy) {
-        double vy = (mag * sin(angle * M_PI / 180)) - (Constants.g * airTime_Deg(mag, angle, dy));
-        return makeVector_Deg(mag * cos(angle * M_PI / 180), vy).second;
-    }
-
-    double landingVelocity_Deg(double mag, double angle, double dy) {
-        double vy = (mag * sin(angle * M_PI / 180)) - (Constants.g * airTime_Deg(mag, angle, dy));
-        return makeVector_Rad(mag * cos(angle * M_PI / 180), vy).first;
-    }
-
-    template <typename ArithType> std::pair<ArithType, double> landingVector(ArithType mag, double angle, ArithType dy, bool useRadians = true) {
+    template <typename ArithType> ArithType peakTime(const ArithType &mag, const double &angle, const bool &useRadians = true) {
         static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-        const double vy = (mag * std::sin(angle) - (Constants.g * airTime<ArithType>(mag, angle, dy, useRadians)));
-        return makeVector<ArithType>(mag * std::cos(angle * (useRadians ? 1 : M_PI / 180)), vy);
+        return std::is_integral<ArithType>::value ? std::round((mag * std::sin(angle * (useRadians ? 1 : M_PI / 180))) / Constants.g) : ((mag * std::sin(angle * (useRadians ? 1 : M_PI / 180))) / Constants.g);
+    }
+
+    template <typename ArithType> ArithType maxHeight(const ArithType &mag, const double &angle, const ArithType &y0, const bool &useRadians = true) {
+        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
+        const double peak = peakTime<double>(mag, angle, useRadians);
+        return std::is_integral<ArithType>::value ? std::round(y0 + mag * std::sin(angle * (useRadians ? 1 : M_PI / 180)) * peak - 0.5 * Constants.g * peak * peak) : (y0 + mag * std::sin(angle * (useRadians ? 1 : M_PI / 180)) * peak - 0.5 * Constants.g * peak * peak);
+    }
+
+    template <typename ArithType> double launchAngle(const ArithType &dx, const ArithType &dy, const ArithType &mag, const bool &minimizeHeight = true, const bool &useRadians = true) {
+        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
+        const double root = std::sqrt(mag * mag * mag * mag - Constants.g * (Constants.g * dx * dx + 2 * dy * mag * mag));
+        const double a1 = std::atan2(mag * mag + root, Constants.g * dx);
+        const double a2 = std::atan2(mag * mag - root, Constants.g * dx);
+        if (minimizeHeight) {
+            if (maxHeight<ArithType>(mag, a1, 0, useRadians) < maxHeight<ArithType>(mag, a2, 0, useRadians)) {return a1 * (useRadians ? 1 : 180 / M_PI);}
+            return a2 * (useRadians ? 1 : 180 / M_PI);
+        }
+        if (maxHeight<ArithType>(mag, a1, 0, useRadians) < maxHeight<ArithType>(mag, a2, 0, useRadians)) {return a2 * (useRadians ? 1 : 180 / M_PI);}
+        return a1 * (useRadians ? 1 : 180 / M_PI);
+    }
+
+    template <typename ArithType> std::pair<ArithType, double> landingVector(const ArithType &mag, const double &angle, const ArithType &dy, const bool &useRadians = true) {
+        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
+        return makeVector<ArithType>(std::is_integral<ArithType>::value ? std::round(mag * std::cos(angle * (useRadians ? 1 : M_PI / 180))) : (mag * std::cos(angle * (useRadians ? 1 : M_PI / 180))), mag * std::sin(angle * (useRadians ? 1 : M_PI / 180)) - Constants.g * airTime<double>(mag, angle, dy, useRadians), useRadians);
     }
 }
 
